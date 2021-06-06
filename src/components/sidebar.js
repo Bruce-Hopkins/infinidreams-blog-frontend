0aac52ce12284173df690ea86668c3d501be9a81
Bruce-Hopkins <behopkinsjr@gmail.com>
2021-06-06 10:17:03 +0200

    Changed HTML for sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -50,29 +50,19 @@ function Sidebar (){
     <SidebarContext.Provider  value={postsData ? postsData : null}>
 
       <nav className={show ? "nav-container show" : "nav-container"}>
-          <svg onClick={showMenu} fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 26 26" width="26px" height="26px"><path d="M 0 4 L 0 6 L 26 6 L 26 4 Z M 0 12 L 0 14 L 26 14 L 26 12 Z M 0 20 L 0 22 L 26 22 L 26 20 Z"/></svg>
         <div className="nav-group">
-          <div className="sections-group">
-            <div className="pages-container">
-              <h3> Pages:</h3>
-              <div className="important-link-group"> 
-                <a href="/"> Home</a>
-                <a href="http://www.infinidream.net/"> About Me</a>
-              </div>
-            </div> 
-            <div className="recent-posts-container">
-              <h3>Recent posts:</h3>
-              <ul className="recent-posts-group">
-                <GetRecentPosts/>
-              </ul>
-            </div> 
-
-          </div>
-
-          <div className="icon-group">
-            <a href="https://twitter.com/InfiniDreams1" target="_blank" rel="noopener noreferrer" > <img src={TwitterIcon} alt="Twitter icon"/> </a>
-            <a href="https://github.com/Bruce-Hopkins-Jr" target="_blank" rel="noopener noreferrer">  <img src={GithubIcon} alt="Github Icon"/> </a>
-            <a href="mailto: sales@budgetchamp.net" target="_blank" rel="noopener noreferrer"> <img src={EmailIcon} alt="Email Icon"/></a>  
+          <div className="top-sidebar">
+            <div className="sidebar-link-group"> 
+              <h2>Pages</h2>
+              <a href="/"> Home</a>
+              <a href="http://www.infinidream.net/"> About Me</a>
+            </div>
+          </div> 
+          <div className="Middle-sidebar">
+            <div className="recent-post-group">
+              <h2>Recent</h2>
+              <GetRecentPosts/>
+            </div>
           </div>
         </div>
       </nav>
