513668e7ec57b2e004edc5326044756ca4b09f44
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2021-06-24 21:19:18 +0200

    added mobile sidebar

--- src/components/navbar/mobileNavbar.jsx
+++ src/components/navbar/mobileNavbar.jsx
@@ -32,7 +32,7 @@ function MobileNavbar (){
 
       <nav className="mobile-menu-container">
         <div className="mobile-menu-group">
-            <a className="mobile-logo-container"href="http://www.infinidream.net/"> <img alt="Logo" className="logo" src={Logo}/> </a>
+            <a className="mobile-logo-container"href="/"> <img alt="Logo" className="logo" src={Logo}/> </a>
             <button onClick={showMenu}>
               <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M0 2.41666V4.83332H29V2.41666H0ZM0 13.2917V15.7083H29V13.2917H0ZM0 24.1667V26.5833H29V24.1667H0Z" fill="black"/>
@@ -42,9 +42,23 @@ function MobileNavbar (){
       </nav>
 
       <div className={openMenu ? "show cover" : "cover" }>
-        <div className="side-menu">
-          <div className="">
-            
+        <div className="side-menu-container">
+          <div className={"side-menu-group"}>
+            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M30 10L10 30" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M10 10L30 30" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <div className="mobile-pages-section"> 
+              <h2> Pages</h2>
+              <a href="/"> Home</a>
+              <a href="http://www.infinidream.net/"> About Me</a>
+            </div>
+            <div className="mobile-recent-posts">
+              <h2> Recent Posts</h2>
+              <GetRecentPosts/>
+            </div>
+
+
           </div>
 
         </div>
