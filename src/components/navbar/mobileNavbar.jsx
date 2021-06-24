4dbdc44ab790d4c6cb288385eb89fdbb9431c7d1
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2021-06-24 21:36:01 +0200

    Added links and button to side-menu

--- src/components/navbar/mobileNavbar.jsx
+++ src/components/navbar/mobileNavbar.jsx
@@ -44,10 +44,13 @@ function MobileNavbar (){
       <div className={openMenu ? "show cover" : "cover" }>
         <div className="side-menu-container">
           <div className={"side-menu-group"}>
-            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M30 10L10 30" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M10 10L30 30" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+            <button onClick={showMenu}>
+              <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M30 10L10 30" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10 10L30 30" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </button>
+
             <div className="mobile-pages-section"> 
               <h2> Pages</h2>
               <a href="/"> Home</a>
