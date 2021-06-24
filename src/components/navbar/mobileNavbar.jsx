bf23ba0372b2a5c093e4773d6ea273ec4e093187
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2021-06-24 20:52:16 +0200

    Changed files to jsx files

--- src/components/navbar/mobileNavbar.jsx
+++ src/components/navbar/mobileNavbar.jsx
@@ -28,20 +28,29 @@ function MobileNavbar (){
   }
 
   return (
-      
-      <section className="mobile-menu-container">
-          <div className="mobile-menu-group">
-              <a className="mobile-logo-container"href="http://www.infinidream.net/"> <img alt="Logo" className="logo" src={Logo}/> </a>
-              <button onClick={showMenu}>
-                <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 2.41666V4.83332H29V2.41666H0ZM0 13.2917V15.7083H29V13.2917H0ZM0 24.1667V26.5833H29V24.1667H0Z" fill="black"/>
-                </svg>
-              </button>
-          </div>
-          <div className={openMenu ? "show mobile-side-menu" : "hide mobile-side-menu" }>
+    <div>
 
+      <nav className="mobile-menu-container">
+        <div className="mobile-menu-group">
+            <a className="mobile-logo-container"href="http://www.infinidream.net/"> <img alt="Logo" className="logo" src={Logo}/> </a>
+            <button onClick={showMenu}>
+              <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 2.41666V4.83332H29V2.41666H0ZM0 13.2917V15.7083H29V13.2917H0ZM0 24.1667V26.5833H29V24.1667H0Z" fill="black"/>
+              </svg>
+            </button>
+        </div>
+      </nav>
+
+      <div className={openMenu ? "show cover" : "cover" }>
+        <div className="side-menu">
+          <div className="">
+            
           </div>
-      </section>
+
+        </div>
+      </div>
+    </div>  
+
   )
 }
 
